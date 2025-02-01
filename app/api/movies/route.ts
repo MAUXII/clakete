6e9de29a0ef905453479edc01e752e626cd5eb36
MAUXII <male.sil2005@gmail.com>
2025-02-01 07:39:01 -0300

    first commit

---
+++ app/api/movies/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server';
+import axios from 'axios';
+
+const TMDB_API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY; 
+const TMDB_BASE_URL = process.env.NEXT_PUBLIC_TMDB_BASE_URL;
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const page = searchParams.get('page') || '1';
+
+  try {
+    const response = await axios.get(`${TMDB_BASE_URL}/movie/popular`, {
+      params: {
+        api_key: TMDB_API_KEY,
+        language: 'pt-BR',
+        page: page,
+      },
+    });
+
+    return NextResponse.json(response.data);
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Erro ao buscar filmes' },
+      { status: 500 }
+    );
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,133 @@
-import Image from "next/image";
+'use client'
+import { useEffect, useState } from 'react';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
+
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+ 
+
+const totalPages = 10;
+interface Movie {
+  id: number;
+  title: string;
+  poster_path: string;
+  overview: string;
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [page, setPage] = useState(1);
+  const { setTheme } = useTheme()
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(`/api/movies?page=${page}`);
+        const data = await response.json();
+        setMovies(data.results);
+      } catch (error) {
+        console.error('Erro ao buscar filmes:', error);
+      }
+    };
+
+    fetchMovies();
+  }, [page]);
+
+  return (
+    <main className='flex w-full min-h-screen items-center justify-center dark:bg-[#09090B]  '>
+    <section className='w-full flex h-full min-h-screen  items-start justify-center pt-24 py-10 md:py-10 md:pt-10 pb-6 px-8'>
+    <div className='flex flex-col gap-3 w-[940px]'>
+    <div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>  
+      </div>
+      <div className='flex flex-col'>
+      <h2 className=' text-2xl font-bold FilmeSans'>Explorar</h2>
+      <p className='dark:text-gray-300 text-gray-600'>Aventure-se em uma vasta coleção de filmes de diversos gêneros.</p>
+      </div>
+      
+      <div className='grid grid-cols-3 lg:grid-cols-5 gap-3'>
+        {movies.map((movie) => (
+          <div key={movie.id} className='w-full border-[1px] border-white/15 h-full relative shadow-sm shadow-white/5 shadow-lg shadow-white/5 aspect-[2/3] rounded-[5px] overflow-hidden '>
+            <div className='border-[2.5px] border-black/10 absolute w-full h-full hover:border-black/40 transition-all duration-700 '></div>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+              className='  w-full h-full object-cover'
+              
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            
+            
+          </div>
+        ))}
+      </div>
+      <Pagination>
+  <PaginationContent className='w-full flex justify-between'>
+    <PaginationItem>
+      <PaginationPrevious className='cursor-pointer' onClick={() => {
+        setPage(page > 1 ? page - 1 : 1);
+        window.scrollTo({ 
+          top: 0,
+          behavior: 'smooth' 
+        });
+      }}
+        />
+    </PaginationItem>
+    
+    <PaginationItem>
+      <PaginationEllipsis />
+    </PaginationItem>
+    <PaginationItem>
+      <PaginationNext className='cursor-pointer' 
+      onClick={() => {
+        setPage(page + 1); 
+        
+        window.scrollTo({ 
+          top: 0,
+          behavior: 'smooth' 
+        });
+      }}  
+    />
+    </PaginationItem>
+  </PaginationContent>
+</Pagination>
+      </div>
+      
+    </section>
+    </main>
   );
 }
