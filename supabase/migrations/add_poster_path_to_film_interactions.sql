eabf7051f3ceb98efd842d4c9ff5d822b09a36bc
Maurice from 19th Century <115664412+MAUXII@users.noreply.github.com>
2025-08-28 10:34:53 -0300

    Delete supabase directory
    my mistake <3

--- supabase/migrations/add_poster_path_to_film_interactions.sql
+++
@@ -1,6 +0,0 @@
--- Adiciona a coluna poster_path à tabela film_interactions
-ALTER TABLE film_interactions
-ADD COLUMN poster_path TEXT;
-
--- Adiciona um comentário explicativo
-COMMENT ON COLUMN film_interactions.poster_path IS 'Caminho do poster do filme no TMDB'; 
